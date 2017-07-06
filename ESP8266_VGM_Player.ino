1d71e9ecea514f5a3a7a8e63161c17b1021742d5
Aidan Lawrence <aidan@aidanlawrence.com>
2017-07-06 12:39:47 -0700

    Now reads VGM data from flash memory instead of storing to RAM

--- ESP8266_VGM_Player.ino
+++ ESP8266_VGM_Player.ino
@@ -49,23 +49,23 @@ void SendSNByte(byte b)
   digitalWrite(WE, HIGH);
 }
 
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
+//Read music data from flash memory
+uint8 ICACHE_FLASH_ATTR read_rom_uint8(const uint8* addr)
 {
- return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    uint32 bytes;
+    bytes = *(uint32*)((uint32)addr & ~3);
+    return ((uint8*)&bytes)[(uint32)addr & 3];
 }
 
 unsigned long parseLocation = 64;
 float sampleWait = 1;
-void loop() 
+void ICACHE_FLASH_ATTR loop(void) 
 {
-  //sampleWait = mapfloat(analogRead(A0), 0, 1023, 0.5, 5);
-  //Serial.println(sampleWait);
-
-  switch(music_data[parseLocation])
+  switch(read_rom_uint8(&music_data[parseLocation]))
   {
     case 0x50:
     parseLocation++;
-    SendSNByte(music_data[parseLocation]);
+    SendSNByte(read_rom_uint8(&music_data[parseLocation]));
     break;
     case 0x61: //There seems to be issues with how these delays are timed. Sample rate doesn't match
     {
@@ -74,23 +74,23 @@ void loop()
     parseLocation++;
     for ( int i = 0; i < 2; i++ ) 
     {
-      wait += ( uint32_t( music_data[parseLocation] ) << ( 8 * i ));
+      wait += ( uint32_t( read_rom_uint8(&music_data[parseLocation]) ) << ( 8 * i ));
       parseLocation++;
     }
     Serial.println(wait);
 //    parseLocation++;
-//    wait << music_data[parseLocation];
+//    wait << read_rom_uint8(&music_data[parseLocation]);
 //    parseLocation++;
-//    wait << music_data[parseLocation];
+//    wait << read_rom_uint8(&music_data[parseLocation]);
     while(wait-- > 0) delayMicroseconds(23);
     //for(int i=0; i < wait; i++) delay(0.023);
     break;
     }
     case 0x62:
-    delay(16.67*0.75); //Actual time is 16.67 (1/60 of a second). 0.75 is just to speed songs up a little;
+    delay(16.67); //Actual time is 16.67 (1/60 of a second). 0.75 is just to speed songs up a little;
     break;
     case 0x63:
-    delay(20*0.75);
+    delay(20);
     break;
     case 0x70:
     case 0x71:
@@ -110,8 +110,8 @@ void loop()
     case 0x7F: //There seems to be issues with how these delays are timed. Sample rate doesn't match
     {
       //parseLocation++;
-      Serial.print("Hit "); Serial.println(music_data[parseLocation]);
-      uint32_t s = music_data[parseLocation] & 0x0F;
+      Serial.print("Hit "); Serial.println(read_rom_uint8(&music_data[parseLocation]));
+      uint32_t s = read_rom_uint8(&music_data[parseLocation]) & 0x0F;
       while ( s-- > 0 ) delayMicroseconds(23);
     break;
     }
@@ -125,34 +125,3 @@ void loop()
     parseLocation = 64;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
